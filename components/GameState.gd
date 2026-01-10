0e3ab5888ad873c3e4a9a5fae708be0faac33ec5
Sean Lam <seanl@seancotech.com>
2026-01-10 12:42:24 -0500

    Add some signals and properties to track game state

--- components/GameState.gd
+++ components/GameState.gd
@@ -1,2 +1,23 @@
 # class_name GameState
 extends Node
+
+signal cash_changed
+signal net_worth_changed
+
+const STARTING_CASH: int = 0
+const STARTING_NET_WORTH: int = 0
+const STARTING_DAY: int = 1
+
+var Cash: int = 0
+var NetWorth: int = 0
+var DayCount: int = 1
+
+func clear_state() -> void:
+    Cash = STARTING_CASH
+    NetWorth = STARTING_NET_WORTH
+    DayCount = STARTING_DAY
+
+func recalculate_net_worth() -> void:
+    NetWorth = Cash  # TODO
+
+    net_worth_changed.emit()
