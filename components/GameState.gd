f35776024be7f9896d492e9cf68a51b7cf760cb9
Sean Lam <seanl@seancotech.com>
2026-01-10 12:56:49 -0500

    Make money a float

--- components/GameState.gd
+++ components/GameState.gd
@@ -4,12 +4,12 @@ extends Node
 signal cash_changed
 signal net_worth_changed
 
-const STARTING_CASH: int = 0
-const STARTING_NET_WORTH: int = 0
+const STARTING_CASH: float = 0.0
+const STARTING_NET_WORTH: float = 0
 const STARTING_DAY: int = 1
 
-var Cash: int = 0
-var NetWorth: int = 0
+var Cash: float = 0.0
+var NetWorth: float = 0.0
 var DayCount: int = 1
 
 func clear_state() -> void:
