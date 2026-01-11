781eccfeff8ae158799152bf7b97b7920f2ac842
Sean Lam <seanl@seancotech.com>
2026-01-11 17:01:09 -0500

    Add Portfolio class

--- components/GameState.gd
+++ components/GameState.gd
@@ -9,15 +9,16 @@ const STARTING_NET_WORTH: float = 0
 const STARTING_DAY: int = 1
 
 var Cash: float = 0.0
+var Investments: Portfolio = Portfolio.new()
 var NetWorth: float = 0.0
 var DayCount: int = 1
 
 func clear_state() -> void:
     Cash = STARTING_CASH
+    Investments.clear()
     NetWorth = STARTING_NET_WORTH
     DayCount = STARTING_DAY
 
 func recalculate_net_worth() -> void:
-    NetWorth = Cash  # TODO
-
+    NetWorth = Cash + Investments.value()
     net_worth_changed.emit()
