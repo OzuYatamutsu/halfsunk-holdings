406fa064e4b87fe7dbaaec14a59afe97ab508e5b
Sean Lam <seanl@seancotech.com>
2026-01-11 17:14:01 -0500

    Fixes from code review

--- components/GameState.gd
+++ components/GameState.gd
@@ -5,21 +5,23 @@ signal cash_changed
 signal net_worth_changed
 
 const STARTING_CASH: float = 0.0
-const STARTING_NET_WORTH: float = 0
+const STARTING_DEBT: float = 0.0
+const STARTING_NET_WORTH: float = 0.0
 const STARTING_DAY: int = 1
 
-var Cash: float = 0.0
-var Investments: Portfolio = Portfolio.new()
-var Debt: float = 0.0
-var NetWorth: float = 0.0
-var DayCount: int = 1
+var cash: float = 0.0
+var investments: Portfolio = Portfolio.new()
+var debt: float = 0.0
+var net_worth: float = 0.0
+var day_count: int = 1
 
 func clear_state() -> void:
-    Cash = STARTING_CASH
-    Investments.clear()
-    NetWorth = STARTING_NET_WORTH
-    DayCount = STARTING_DAY
+    cash = STARTING_CASH
+    investments.clear()
+    debt = STARTING_DEBT
+    net_worth = STARTING_NET_WORTH
+    day_count = STARTING_DAY
 
 func recalculate_net_worth() -> void:
-    NetWorth = Cash + Investments.value() - Debt
+    net_worth = cash + investments.value() - debt
     net_worth_changed.emit()
