b9c15b5765c66ecd2853846ebb5e1c5afa0dd63c
Sean Lam <seanl@seancotech.com>
2026-01-14 10:54:58 -0500

    First pass at login screen

---
+++ components/command.gd
@@ -0,0 +1,10 @@
+class_name Command
+extends Node
+
+## Commands are executed by the CommandPrompt.
+
+var keyword = "COMMAND"
+
+## What should happen when this command is executed?
+func do(raw_args: String) -> void:
+    pass
--- components/command_prompt.gd
+++ components/command_prompt.gd
@@ -1,5 +1,11 @@
 class_name CommandPrompt
 extends Control
 
+@onready var PromptEdit: LineEdit = $PromptEdit
+
 func _ready() -> void:
     pass
+
+func _on_prompt_edit_text_submitted(new_text: String) -> void:
+    # TODO parse and read command
+    PromptEdit.clear()
