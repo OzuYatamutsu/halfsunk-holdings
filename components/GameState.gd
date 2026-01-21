e7813b07d219e48096d0dc2945a7aeb5ef76fdfa
Sean Lam <seanl@seancotech.com>
2026-01-20 20:59:49 -0800

    Populate version string and build date at runtime

--- components/GameState.gd
+++ components/GameState.gd
@@ -4,16 +4,25 @@ extends Node
 signal cash_changed
 signal net_worth_changed
 
+const VERSION_STRING: String = "0.0.1"
 const STARTING_CASH: float = 0.0
 const STARTING_DEBT: float = 0.0
 const STARTING_NET_WORTH: float = 0.0
 const STARTING_DAY: int = 1
 
+var BUILD_DATE: String = ""
 var cash: float = 0.0
 var investments: Portfolio = Portfolio.new()
 var debt: float = 0.0
 var net_worth: float = 0.0
 var day_count: int = 1
+
+func _init() -> void:
+    _populate_build_date()
+
+func _populate_build_date() -> void:
+    var date = Time.get_date_dict_from_system()
+    BUILD_DATE = "%04d%02d%02d" % [date.year, date.month, date.day]
 
 func clear_state() -> void:
     cash = STARTING_CASH
