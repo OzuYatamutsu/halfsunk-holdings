bb1c519ec211c999e26b4890ca87297b2cbf7585
Sean Lam <seanl@seancotech.com>
2026-02-21 05:24:26 -0800

    Hook up MainMenu to SettingsMenu

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -21,7 +21,7 @@ func _on_load_game_button_pressed() -> void:
     pass # Replace with function body.
 
 func _on_settings_button_pressed() -> void:
-    pass # Replace with function body.
+    get_tree().change_scene_to_file("res://scenes/SettingsMenu.tscn")
 
 func _on_quit_button_pressed() -> void:
     get_tree().quit()
