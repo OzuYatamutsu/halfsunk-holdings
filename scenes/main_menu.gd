d107293fad1845f19466088e9681e4d5ef35e5bf
Sean Lam <seanl@seancotech.com>
2026-02-22 20:38:39 -0800

    Add link to load game menu

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -18,7 +18,7 @@ func _on_new_game_button_pressed() -> void:
     pass # Replace with function body.
 
 func _on_load_game_button_pressed() -> void:
-    pass # Replace with function body.
+    get_tree().change_scene_to_file("res://scenes/LoadGameMenu.tscn")
 
 func _on_settings_button_pressed() -> void:
     get_tree().change_scene_to_file("res://scenes/SettingsMenu.tscn")
