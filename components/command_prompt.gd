324c5b72f9fbbf94917ec6301a4337ebbd905330
Sean Lam <seanl@seancotech.com>
2026-02-24 18:34:49 -0800

    First set of implementation of commandprompt

--- components/command_prompt.gd
+++ components/command_prompt.gd
@@ -1,11 +1,40 @@
 class_name CommandPrompt
 extends Control
 
+## To add a command handler to the list,
+var COMMAND_LIST: Dictionary[String, Callable] = {
+    "NOOP": _handle_noop
+}
+
 @onready var PromptEdit: LineEdit = $PromptEdit
+@onready var NotFoundStatusText: Label = %NotFoundStatusText
 
 func _ready() -> void:
-    pass
+    NotFoundStatusText.visible = false
 
 func _on_prompt_edit_text_submitted(new_text: String) -> void:
-    # TODO parse and read command
+    if new_text.length() == 0:
+        return
+
+    var command = new_text.split(" ")[0]
+    var command_args = new_text.split(" ")
+    command_args.remove_at(0)
+
+    if !(command in COMMAND_LIST):
+        _handle_command_not_found()
+    else:
+        COMMAND_LIST[command].call(command_args)
     PromptEdit.clear()
+
+func _handle_command_not_found() -> void:
+    # Flash not found text for a tiny amount of time
+    NotFoundStatusText.visible = true
+    var status_flash = create_tween()
+    status_flash.tween_interval(1.0)
+    status_flash.tween_callback(func():
+        NotFoundStatusText.visible = false
+    )
+
+## This command does nothing
+func _handle_noop(_args: Array[String]) -> void:
+    pass
