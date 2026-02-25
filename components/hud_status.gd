25d5baa9a12a9da4664afd297203f1597bc5951d
Sean Lam <seanl@seancotech.com>
2026-02-24 19:05:39 -0800

    Hook up update days, money

---
+++ components/hud_status.gd
@@ -0,0 +1,10 @@
+class_name HudStatus
+extends HBoxContainer
+
+@onready var CalendarDaysLabel: Label = %CalendarDaysLabel
+@onready var MoneyLabel: Label = %MoneyLabel
+
+## Updates the UI with the current state
+func update() -> void:
+    CalendarDaysLabel.text = str(GameState.day_count)
+    MoneyLabel.text = str(GameState.cash)
