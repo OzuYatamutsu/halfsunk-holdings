f451ab2f347b0f258e4549e8be832b1eb12e01bf
Sean Lam <seanl@seancotech.com>
2026-03-01 12:50:41 -0800

    Set up keyboard scroll

--- components/dynamic_page.gd
+++ components/dynamic_page.gd
@@ -5,6 +5,8 @@ extends VBoxContainer
 ## in the page_contents group. Call actually_ready()
 ## after these have been assigned.
 
+const KEYBOARD_SCROLL_SPEED: int = 75
+
 @onready var PageTitleLabel: Label = $DynamicPageTitle/PageTitleLabel
 @onready var DynamicPageScroller: ScrollContainer = $DynamicPageScroller
 @onready var DynamicPageContents: PageContent = $DynamicPageScroller/DynamicPageContents
@@ -22,3 +24,15 @@ func load_page(path_to_page_content: String) -> void:
     DynamicPageScroller.add_child(DynamicPageContents)
     PageTitle = DynamicPageContents.Title
     PageTitleLabel.text = PageTitle
+
+func _unhandled_input(event: InputEvent) -> void:
+    if event.is_action_pressed("ui_up"):
+        keyboard_scroll_up()
+    elif event.is_action_pressed("ui_down"):
+        keyboard_scroll_down()
+
+func keyboard_scroll_up() -> void:
+    DynamicPageScroller.scroll_vertical -= KEYBOARD_SCROLL_SPEED
+
+func keyboard_scroll_down() -> void:
+    DynamicPageScroller.scroll_vertical += KEYBOARD_SCROLL_SPEED
