38087e7c496b346e5bb8a98823bac9b030a79cad
Sean Lam <seanl@seancotech.com>
2026-03-01 12:44:57 -0800

    Update scrollable area code

--- components/page_content.gd
+++ components/page_content.gd
@@ -3,3 +3,10 @@ extends Control
 
 @onready var DynamicPageContent: Control = %DynamicPageContent
 @export var Title: String
+@export var PageHeightY: int
+
+
+## Call this after updating PageHeightY
+## to get scrolling to work properly
+func update_scrollable_area() -> void:
+    custom_minimum_size = Vector2(0, PageHeightY)
