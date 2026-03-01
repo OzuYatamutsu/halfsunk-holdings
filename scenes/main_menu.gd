a99409e63e78b943f20f20b27f39f2b8f26d8515
Sean Lam <seanl@seancotech.com>
2026-03-01 12:33:08 -0800

    Fix broken load game menu functionality

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -18,6 +18,10 @@ func _on_new_game_button_pressed() -> void:
     AudioEngine.play_sfx(AudioEngine.SFX_CLICK)
     get_tree().change_scene_to_file("res://levels/Level0.tscn")
 
+func _on_load_game_button_pressed() -> void:
+    AudioEngine.play_sfx(AudioEngine.SFX_CLICK)
+    get_tree().change_scene_to_file("res://scenes/LoadGameMenu.tscn")
+
 func _on_settings_button_pressed() -> void:
     AudioEngine.play_sfx(AudioEngine.SFX_CLICK)
     get_tree().change_scene_to_file("res://scenes/SettingsMenu.tscn")
