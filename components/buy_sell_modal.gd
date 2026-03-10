87f62fa25b9eb8af844bf5f98a1dc86f780e2137
Sean Lam <seanl@seancotech.com>
2026-03-10 14:47:07 -0700

    Load data on ready

--- components/buy_sell_modal.gd
+++ components/buy_sell_modal.gd
@@ -6,7 +6,8 @@ enum Mode {
     SELL
 }
 
-var CurrentMode: Mode = Mode.BUY
+var ticker: String = ""
+var action: Mode = Mode.BUY
 
 # "Jinhai Holdings (JINH)"
 @onready var TickerLabel: Label = $ModalWindow/InfoContainer/TickerLabel
@@ -33,3 +34,36 @@ var CurrentMode: Mode = Mode.BUY
 
 # "$12,345.00"
 @onready var TransactionValueLabel: Label = $ModalWindow/InfoContainer/TotalInfo/TransactionValueLabel
+
+func _ready() -> void:
+    # Data bus format:
+    # "<ticker>;<action>"
+    # e.g. "JINH;BUY"
+    
+    ticker = GameState.switch_page_data_bus.split(';').to_upper()[0]
+    action = Mode[GameState.switch_page_data_bus.split(';').to_upper()[1]]
+    _update_stock_info()
+
+func _update_stock_info() -> void:
+    var stock = GameState.stock_market.get_stock(ticker)
+    TickerLabel.text = "%s (%s)" % [stock.company_name, stock.ticker_symbol]
+    ValueLabel.text = "%.2f" % stock.current_value
+    UpDownLabel.text = (
+        SharedConstants.UP_SYMBOL if stock.last_delta >= 0 
+        else SharedConstants.DOWN_SYMBOL
+    )
+    UpDownLabel.add_theme_color_override("font_color", Color(
+        SharedConstants.POSITIVE_COLOR_CODE if stock.last_delta >= 0
+        else SharedConstants.NEGATIVE_COLOR_CODE
+    ))
+    
+    NetChangeValueLabel.text = "%.2f" % stock.last_delta
+    NetChangePercentLabel.text = (
+        "("
+        + ("+" if stock.last_delta >= 0 else "-")
+        + "%.2f)" % (100.0 * (stock.last_delta / (stock.last_delta + stock.current_value)))
+    )
+    NetChangePercentLabel.add_theme_color_override("font_color", Color(
+        SharedConstants.POSITIVE_COLOR_CODE if stock.last_delta >= 0
+        else SharedConstants.NEGATIVE_COLOR_CODE
+    ))
