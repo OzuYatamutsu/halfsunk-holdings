41b326cc1d9cd7eec570c059920323bd0688a5ac
Sean Lam <seanl@seancotech.com>
2026-03-11 22:35:06 -0400

    Only add_child for timers once

--- components/page_content.gd
+++ components/page_content.gd
@@ -5,7 +5,6 @@ extends Control
 @export var Title: String
 @export var PageHeightY: int
 
-
 ## Call this after updating PageHeightY
 ## to get scrolling to work properly
 func update_scrollable_area() -> void:
