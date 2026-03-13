90856c82842492d9b694d52fc29b8c5c8b5ae692
Sean Lam <seanl@seancotech.com>
2026-03-13 15:10:53 -0400

    Add new helper function to turn floats into currency strings

--- components/Helpers.gd
+++ components/Helpers.gd
@@ -5,3 +5,32 @@ class_name Helpers
 ## Rounds to the nearest hundredths place.
 static func money_round(x: float) -> float:
     return round(x * 100.0) / 100.0
+
+
+## Formats a float to currency string (xxxx.xxx -> "$x,xxx.xx")
+static func currencyify(raw_number: float) -> String:
+    var is_negative: bool = (raw_number < 0)
+    var number_as_string: String = "%.2f" % [raw_number]
+    number_as_string.replace("-", "")
+    
+    if raw_number < 1000.00 && raw_number > -1000.00:
+        return (
+            "($%s)" % [number_as_string] if is_negative
+            else "$%s" % [number_as_string]
+        )
+    
+    var output_string: String = ""
+    var last_index: int = number_as_string.length() - 4
+
+    # Insert commas as necessary
+    for index in range(number_as_string.length()):
+        output_string = output_string + number_as_string.substr(index, 1)
+        if (last_index - index) % 3 == 0 and index < last_index:
+            output_string = output_string + ","
+
+    # Insert negative numbers
+    if is_negative:
+        output_string = "($%s)" % [output_string]
+    else:
+        output_string = "$%s" % [output_string]
+    return output_string
