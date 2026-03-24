4865f8c3becf09b6bf896409f117a3c3c3414c5a
Sean Lam <seanl@seancotech.com>
2026-03-23 22:27:10 -0400

    Fix easy_charts so git stops complaining

--- addons/easy_charts/examples/simple_chart/Control.gd
+++ addons/easy_charts/examples/simple_chart/Control.gd
@@ -9,23 +9,23 @@ var chart: Chart
 var f1: Function
 
 func _ready():
-	chart = chart_scn.instantiate()
-	$VBoxContainer.add_child(chart)
-	
-	# Let's create our @x values
+    chart = chart_scn.instantiate()
+    $VBoxContainer.add_child(chart)
+    
+    # Let's create our @x values
 	var x: Array = ArrayOperations.multiply_float(range(-10, 11, 1), 0.5)
 	
 	# And our y values. It can be an n-size array of arrays.
 	# NOTE: `x.size() == y.size()` or `x.size() == y[n].size()`
 	var y: Array = ArrayOperations.multiply_int(ArrayOperations.cos(x), 20)
 	
-	# Let's add values to our functions
-	f1 = Function.new(x, y, "Pressure", { marker = Function.Marker.CIRCLE })
-	
-	# Set fixed Y domain
-	chart.set_y_domain(-50, 50)
-	
-	# Now let's plot our data
+    # Let's add values to our functions
+    f1 = Function.new(x, y, "Pressure", { marker = Function.Marker.CIRCLE })
+    
+    # Set fixed Y domain
+    chart.set_y_domain(-50, 50)
+    
+    # Now let's plot our data
 	chart.plot([f1])
 	
 	# Uncommenting this line will show how real time data plotting works
