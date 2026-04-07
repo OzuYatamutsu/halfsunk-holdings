1010f106e48dfe1a25240393f315186bf8a47164
Sean Lam <seanl@seancotech.com>
2026-04-07 00:06:34 -0400

    Fire events on tick count

--- levels/level_0.gd
+++ levels/level_0.gd
@@ -1,9 +1,24 @@
 extends Control
 
+var _events: Dictionary[int, Callable] = {10: _event_chat_message}
+
 @onready var window: GameWindow = $GameWindow
 
 func _ready() -> void:
     AudioEngine.play_bgm(AudioEngine.BGM_GAME)
     GameState.clear_state()
     GameState.start_day()
+    GameState.tick.connect(_on_tick)
     window.browser.load_page("res://pages/StartPage.tscn")
+
+func _on_tick() -> void:
+    if GameState.tick_count in _events:
+        print(
+            "event: firing %s" % [
+                _events[GameState.tick_count].get_method()
+            ]
+        )
+        _events[GameState.tick_count].call()
+
+func _event_chat_message() -> void:
+    pass  # TODO
