395c0573ed29f22e7864e0ef5c96a183acf235b8
Sean Lam <seanl@seancotech.com>
2026-04-08 08:03:01 -0400

    Expose user info options

--- components/chat_window_modal.gd
+++ components/chat_window_modal.gd
@@ -6,16 +6,30 @@ const ChatMessageComponent: PackedScene = preload("res://components/chat_message
 ## Controls what the text on the bottom will be.
 ## If only one item is set, the other button will be disabled.
 @export var ButtonOptions: Array[String] = ["Yes", "No"]
+@export var UserName: String = "Jeff Someguy"
+@export var UserTitle: String = "Investment Advisor at Jinhai Holdings"
+@export var UserProfilePath: String = "res://components/chat_user_icon_100px.png"
 
 @onready var chat_messages: VBoxContainer = $ModalWindow/InfoContainer/ChatContainer/ChatMessages
 @onready var _test_chat_message: ChatMessage = $ModalWindow/InfoContainer/ChatContainer/ChatMessages/_test_chat_message
 @onready var yes_button: Button = %YesButton
 @onready var no_button: Button = %NoButton
 
+@onready var _chat_user_profile: TextureRect = %ChatUserProfile
+@onready var _chat_user_name: Label = %ChatUserName
+@onready var _chat_user_title: Label = %ChatUserTitle
+
 
 func _ready() -> void:
     _test_chat_message.queue_free()
+    update_user_info()
     update_button_options()
+
+
+func update_user_info() -> void:
+    _chat_user_profile.texture = load(UserProfilePath)
+    _chat_user_name.text = UserName
+    _chat_user_title.text = UserTitle
 
 
 func update_button_options() -> void:
