ba437d819425c342d43b21a5bdadd24779810a8d
Sean Lam <seanl@seancotech.com>
2026-04-08 08:39:08 -0400

    Chat message factory

--- levels/level_0.gd
+++ levels/level_0.gd
@@ -7,9 +7,5 @@ func _ready() -> void:
     _events = {1: _event_chat_message}
 
 func _event_chat_message() -> void:
-    var chat_window: ChatWindowModal = load("res://components/ChatWindowModal.tscn").instantiate()
+    var chat_window: ChatWindowModal = ChatWindowModal.Create("res://events/day0_test_message1.gd")
     window.add_child(chat_window)
-    chat_window.preload_chat_messages([
-        str(GameState.get_current_timestamp()) + " hi want some terrible investment advice"
-    ])
-    AudioEngine.play_sfx(AudioEngine.SFX_MESSAGE_RECEIVED)
