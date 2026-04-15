f63bb258f3216cdd2141cec2dc03dc865b5ec1d7
Sean Lam <seanl@seancotech.com>
2026-04-14 21:38:48 -0400

    Refactor level0 as Day

--- levels/level_0.gd
+++ levels/level_0.gd
@@ -1,13 +1,23 @@
-extends Level
-
+extends Day
 
 
 func _ready() -> void:
     super()
-    _events = {1: _event_chat_message}
+
+    events = {Day.Phase.PREMARKET: _event_chat_message}
+
+
+func on_marketopen_start() -> void:
+    super()
+
+    AudioEngine.play_bgm(AudioEngine.BGM_GAME)
+    GameState.game_window.browser.load_page(
+        "res://pages/StartPage.tscn"
+    )
+
 
 func _event_chat_message() -> void:
     var chat_window: ChatWindowModal = ChatWindowModal.Create(
         "res://events/day0_test_message1.gd"
     )
-    window.add_child(chat_window)
+    GameState.game_window.add_child(chat_window)
