26c70948f9ab68199e64b005c018a06f9b313586
Sean Lam <seanl@seancotech.com>
2026-04-27 22:01:45 -0400

    Hook up modal buttons

--- components/game_over_modal.gd
+++ components/game_over_modal.gd
@@ -8,3 +8,13 @@ extends Modal
 func _ready() -> void:
     total_score_value.text = Helpers.currencyify(GameState.total_score, false, false)
     job_held_for_value.text = "%s days" % [GameState.day_count]
+
+
+func _on_main_menu_button_pressed() -> void:
+    print("return to main menu button pressed")
+    get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+    print("quit button pressed")
+    get_tree().quit()
