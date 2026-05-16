c5cca1482f6f9145915f568ebbb52c2914a61603
Sean Lam <run@slamtech.dev>
2026-05-16 12:57:09 -0400

    Fix BGM bug

--- days/day0002.gd
+++ days/day0002.gd
@@ -14,6 +14,7 @@ func _ready() -> void:
 
 func on_premarket_start() -> void:
     super()
+    AudioEngine.play_bgm(AudioEngine.BGM_GAME)
     GameState.game_window.browser.load_page("res://pages/StartPage.tscn")
 
 
