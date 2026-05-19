f7063f7af7bb4cfbb4f980e144180830b1472a68
Sean Lam <run@slamtech.dev>
2026-05-18 22:58:41 -0400

    Enter advances dialog via the yes option; fixes #68

--- components/modal_window.gd
+++ components/modal_window.gd
@@ -5,3 +5,7 @@ extends Window
 func _input(event: InputEvent) -> void:
     if event.is_action_pressed("ui_cancel"):
         close_requested.emit()
+    if event.is_action_pressed("ui_accept"):
+        # Enter key should default to whatever
+        # the yes option is
+        get_parent()._on_yes_button_pressed()
