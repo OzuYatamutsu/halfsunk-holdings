fdde3901a6bc0166e9afeca59b7d48f7d39b807d
Sean Lam <run@slamtech.dev>
2026-05-20 09:26:53 +0200

    Fix fake hud status in anim

--- components/hud_status.gd
+++ components/hud_status.gd
@@ -1,8 +1,12 @@
 class_name HudStatus
 extends HBoxContainer
 
+const FRONT_LAYER = 10
+const DEFAULT_LAYER = 1
+
 @onready var CalendarDaysLabel: Label = %CalendarDaysLabel
 @onready var MoneyLabel: Label = %MoneyLabel
+@onready var icon_money: TextureRect = $IconMoney
 
 
 func _ready():
@@ -13,3 +17,18 @@ func _ready():
 func update() -> void:
     CalendarDaysLabel.text = GameState.get_current_timestamp_humanized()
     MoneyLabel.text = Helpers.currencyify(GameState.cash)
+
+
+## Sets the days label to an arbitrary value.
+func set_days_arbitrary(text: String) -> void:
+    CalendarDaysLabel.text = text
+
+
+func hide_money_label() -> void:
+    icon_money.visible = false
+    MoneyLabel.visible = false
+
+
+func show_money_label() -> void:
+    icon_money.visible = true
+    MoneyLabel.visible = true
