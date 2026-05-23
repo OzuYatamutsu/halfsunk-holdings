31390136ffb6d9f9fa133463f1c1f2ab4b9d5868
Sean Lam <run@slamtech.dev>
2026-05-23 14:55:52 +0200

    Add queueing behaviour to ticker_tape

--- components/ticker_tape.gd
+++ components/ticker_tape.gd
@@ -5,15 +5,28 @@ extends Control
 
 @onready var label: Label = %Label
 
+var _tween = create_tween()
+var _is_active: bool = false
+var _marquee_queue: Array[String] = []
+
+
+func _ready() -> void:
+    _tween.finished.connect(_on_marquee_finished)
+
+
+func queue_text(text: String) -> void:
+    _marquee_queue.append(text)
+    if (!_is_active):
+        start_marquee()
+
+
 func set_text(text: String) -> void:
     label.text = text
 
 
-func start_marquee() -> void:
-    # Wait one frame so sizes are correct
-    await get_tree().process_frame
+func start_marquee() -> void:    
+    _is_active = true
 
-    var tween = create_tween()
     var y := label.position.y
     
     # Start just off the right side
@@ -23,9 +36,17 @@ func start_marquee() -> void:
     )
 
     # Move label from right to left
-    tween.tween_property(
+    _tween.tween_property(
         label,
         "position:x",
         -label.size.x,
         ((get_viewport_rect().size.x + label.size.x) / speed_px_per_sec)
     ).set_trans(Tween.TRANS_LINEAR)
+
+
+func _on_marquee_finished() -> void:
+    if (!_marquee_queue.is_empty()):
+        set_text(_marquee_queue.pop_front())
+        start_marquee()
+    else:
+        _is_active = false
