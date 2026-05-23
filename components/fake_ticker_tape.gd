77c60a983ec8f8d1e01859a1fb6de5a43121d21b
Sean Lam <run@slamtech.dev>
2026-05-23 15:20:52 +0200

    Address code review comments

--- components/fake_ticker_tape.gd
+++ components/fake_ticker_tape.gd
@@ -4,7 +4,6 @@ extends Control
 const DUMMY_TEXT := "CAT 223.25 ↗ 22.05 (+10.96%)  |  BIRD 415.28 ↗ 22.05 (+5.61%)  |  DOG 15.26 ↘ 1.06 (−6.50%)  |  CRW 12.48 ↘ 1.14 (−8.37%)  |  SNEK 66.66 ↗ 0.22 (+0.33%)  |  TIGR 21.14 ↗ 0.22 (+1.05%)  |  LZRD 67.69 ↗ 0.69 (+1.03%)  |  "
 
 @export var speed_px_per_sec: float = 100.0
-@export var text: String = ""
 
 @onready var _label: Label = %Label
 @onready var _label2: Label = %Label2
@@ -17,13 +16,12 @@ const DUMMY_TEXT := "CAT 223.25 ↗ 22.05 (+10.96%)  |  BIRD 415.28 ↗ 22.05 (+
 var _running := true
 
 func _ready():
-    text = (DUMMY_TEXT)
     await _apply_text()
     _reset_positions()
 
 func _apply_text():
-    _label.text = text
-    _label2.text = text
+    _label.text = DUMMY_TEXT
+    _label2.text = DUMMY_TEXT
 
     await get_tree().process_frame
     speed_px_per_sec = _label.size.x / 10.0
