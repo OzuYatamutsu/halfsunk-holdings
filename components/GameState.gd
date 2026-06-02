1d180e04f874829167de7660862e8e110634a640
Sean Lam <run@slamtech.dev>
2026-06-02 10:27:56 +0200

    Bump version

--- components/GameState.gd
+++ components/GameState.gd
@@ -6,7 +6,7 @@ signal net_worth_changed
 
 
 const BUILD_DATE: String = "20260602"
-const VERSION_STRING: String = "0.4.13"
+const VERSION_STRING: String = "0.4.14"
 const SAVE_GAME_PATH_ROOT: String = "user://"
 const SAVE_GAME_PATH_FOLDER: String = "savegames"
 const SAVE_GAME_PATH: String = SAVE_GAME_PATH_ROOT + SAVE_GAME_PATH_FOLDER
