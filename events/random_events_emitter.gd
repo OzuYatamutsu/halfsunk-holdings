cda436bbc0a886b3a1daba68b04a4878feb1da3c
Sean Lam <run@slamtech.dev>
2026-07-16 20:14:19 +0800

    Add cm_random_coffee01 chat event

--- events/random_events_emitter.gd
+++ events/random_events_emitter.gd
@@ -11,6 +11,7 @@ static var MewEvent01: String = "res://events/cm_random_mew_event01.gd"
 static var RandomGold01: String = "res://events/cm_random_gold01.gd"
 static var OptionsTrading01: String = "res://events/cm_random_options01.gd"
 static var ParasolVirus01: String = "res://events/cm_random_parasol_virus01.gd"
+static var CoffeeEvent01: String = "res://events/cm_random_coffee01.gd"
 
 static var EventsNotFired: Array[String]
 
@@ -19,7 +20,7 @@ static var EventsNotFired: Array[String]
 static func repopulateEvents() -> void:
     EventsNotFired = [
         CatInsideTrading01, MewEvent01, RandomGold01,
-        OptionsTrading01, ParasolVirus01
+        OptionsTrading01, ParasolVirus01, CoffeeEvent01
     ]
 
 
