799fc626aa9658b9b33f2dbd7ecf19b4f91044dd
Sean Lam <run@slamtech.dev>
2026-07-16 20:14:19 +0800

    Add cm_random_coffee01 chat event

---
+++ events/cm_random_coffee01.gd
@@ -0,0 +1,29 @@
+extends ChatWindowModal
+
+
+func _ready() -> void:
+    IgnoreCloseRequests = true
+    ButtonOptions = ["OK!", "Maybe not..."]
+    UserName = "Jordan Belmont"
+    UserTitle = "Bioresearcher at Jinhai Holdings"
+    UserProfilePath = "res://components/pfp_awre_twelve.png"
+    ChatMessages = [
+        "%TS/Let's meet for coffee at EXPENSIVE RESTAURANT."
+    ]
+    YesAction = _0_on_yes_button_pressed
+    NoAction = _close_window
+
+    super._ready()
+
+
+func _0_on_yes_button_pressed() -> void:
+    ButtonOptions = ["(close)"]
+    YesAction = _close_window
+    update_button_options()
+
+    add_message("%TS/[Expensed EXPENSIVE COFFEE (-$100.)]")
+
+
+func _close_window() -> void:
+    IgnoreCloseRequests = false
+    close()
